808cb4545fd62d3b14239677d1021cb09eb88d13
Danesh Tolani <danesh.tolani@gmail.com>
2022-11-01 19:12:09 +0530

    added banner section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/1920px-Netflix_2014_logo.svg.png" alt="" className="nav_logo" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="" className="nav_avatar" />
+    </div>
+  );
 }
 
 export default Navbar;
