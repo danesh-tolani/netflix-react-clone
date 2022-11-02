b40ce232a05f066b29ce6530bc7559cd07398cf6
Danesh Tolani <danesh.tolani@gmail.com>
2022-11-02 12:17:32 +0530

    added Trailer popout

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import axios from "../axios";
 import "./Row.css";
 
@@ -8,6 +9,7 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState("");
 
   // a snippet of code which runs based on a specific condition/variable
   useEffect(() => {
@@ -20,19 +22,46 @@ function Row({ title, fetchURL, isLargeRow }) {
     fetchData();
   }, [fetchURL]); // when ever there is an external variable used inside useEffect (fetchURL in the below example), we need to put that in the dependency array
 
-  const opts = {};
+  // video playback options
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerURL) {
+      setTrailerURL("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerURL(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   // console.table(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {/* row_poster */}
         {movies.map((movie) => {
-          return <img key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarger"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />;
+          return (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row_poster ${isLargeRow && "row_posterLarger"}`}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          );
         })}
       </div>
-      {/* <YouTube videoId={trailerURL} opts={opts} /> */}
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   );
 }
